26e6eba99e167db3cdcc3f9015819dd847ab75c2
name <my@email>
2019-03-27 00:27:58 +0300

    fix

--- app/Http/Controllers/ApiV1/Controller.php
+++ app/Http/Controllers/ApiV1/Controller.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\ApiV1;
 
 use App\Http\Controllers\Controller as BaseController;
 
-class Controller extends BaseController
+abstract class Controller extends BaseController
 {
     public function responseSuccess($data = []){
 		return response()->json(['result'=>'success'] + $data);
